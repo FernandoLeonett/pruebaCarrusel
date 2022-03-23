110569ed81ca5e9e7f8c2db38d1a1a5126d32417
fernandoleonett <68520609+FernandoLeonett@users.noreply.github.com>
2022-03-23 19:02:47 -0300

    Primer commit carrusel prueba

---
+++ carusel.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('.center').slick({
+		infinite:true,
+		slidesToShow: 1,
+    slidesToScroll: 1,
+		arrows:false,
+		autoplay: true,
+		autoplaySpeed: 3900,
+		dots: false,
+
+  
+       
+	});
+});
